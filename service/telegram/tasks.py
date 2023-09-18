ece8849109159ee18b278f6325d8973e28072de0
Артем Зайцев <azaitsev@astralinux.ru>
2023-09-19 02:10:36 +0300

    fix

--- service/telegram/tasks.py
+++ service/telegram/tasks.py
@@ -31,7 +31,7 @@ api_hash = '4d8880bc0cfa9c67838f03c21edcf3f7'
 session='anon'
 openai.api_key = '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'
 GPT_MODEL='gpt-4'
-MAX_TOKENS = 7000
+MAX_TOKENS = 6000
 NOTIFY_TOKEN='cjc5oad_h4jqmn6eb2-7tmg7j8r7ly4o'
 
 def get_msk_time(time: datetime):
